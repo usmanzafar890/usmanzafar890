d3d6569e15f552d6d645395fcdaa55966e73f954
Usman Zafar <114914063+usmanzafar890@users.noreply.github.com>
2024-08-14 13:01:51 +0500

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/',(req,res)=>{
+    try {
+        res.send({message:"bhai kam ho gya hy dont worry"})
+    } catch (error) {
+        res.send(error)
+    }
+})
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
